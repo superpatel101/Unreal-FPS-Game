f06de08d2ae6175e43f206aa5fc6d7efdf9d5d4f
James Xu <xujames007@gmail.com>
2019-06-11 14:50:58 -0400

    Bot fire beginnings and server checks

--- Source/COOP_Game_v3/Private/BotCharacter.cpp
+++ Source/COOP_Game_v3/Private/BotCharacter.cpp
@@ -7,58 +7,105 @@
 #include "Kismet/GameplayStatics.h"
 #include "NavigationSystem/Public/NavigationSystem.h"
 #include "NavigationSystem/Public/NavigationPath.h"
+#include "Public/SWeapon.h"
 
 void ABotCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+    
+    if (Role == ROLE_Authority)
+    {
+        TArray<AActor*> FoundActors;
+        
+        UGameplayStatics::GetAllActorsOfClass(GetWorld(), ASCharacter::StaticClass(), FoundActors);
+        
+        AActor* Nearest = nullptr;
+        
+        for (int i = 0; i < FoundActors.Num(); i++)
+        {
+            if (FoundActors[i] != this && !Cast<ABotCharacter>(FoundActors[i]))
+            {
+                if (!Nearest)
+                {
+                    Nearest = FoundActors[i];
+                }
+                float distOld = Nearest->GetDistanceTo(this);
+                float distNew = FoundActors[i]->GetDistanceTo(this);
+                
+                if (distNew < distOld)
+                {
+                    Nearest = FoundActors[i];
+                }
+            }
+        }
+        if (Nearest)
+        {
+            
+            UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(GetWorld(), GetActorLocation(), Nearest);
+            
+            if (NavPath && NavPath->PathPoints.Num() > 1)
+            {
+                FVector NextPoint = NavPath->PathPoints[1];
+                FVector Direction = NextPoint - GetActorLocation();
+                Direction.Normalize();
+//                GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Yellow, GetControlRotation().ToString());
+                FRotator Rotation = Direction.Rotation();
+                Rotation.Pitch = 0;
+                Rotation.Roll = 0;
+                SetActorRotation(Rotation);
+                if ((Nearest->GetActorLocation() - GetActorLocation()).Size() > 300)
+                {
+                    MoveForward(80 * DeltaTime);
+                }
+            }
+            if (GetWorld()->GetRealTimeSeconds() - TimeLastFired > 1)
+            {
+                Fire();
+                TimeLastFired = GetWorld()->GetRealTimeSeconds();
+            }
+        }
+        
+    } else
+    {
+//        UE_LOG(LogTemp, Warning, TEXT("(BOT) Tick failed: not ROLE_Authority"));
 
-	TArray<AActor*> FoundActors;
+    }
+}
 
-	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ASCharacter::StaticClass(), FoundActors);
+void ABotCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+    if (Role != ROLE_Authority)
+    {
+        ServerBeginPlay();
+    } else
+    {
+        TimeLastFired = GetWorld()->GetRealTimeSeconds();
+        
+        UE_LOG(LogTemp, Warning, TEXT("Now server!"));
+        FActorSpawnParameters SpawnParams;
+        SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+        
+        
+        CurrentWeapon = GetWorld()->SpawnActor<ASWeapon>(StarterWeaponClass, FVector::ZeroVector, FRotator::ZeroRotator, SpawnParams);
+        
+        if (CurrentWeapon) {
+            
+            CurrentWeapon->SetOwner(this);
+            CurrentWeapon->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, WeaponAttachSocketName);
+        }
+        else {
+            UE_LOG(LogTemp, Warning, TEXT("Current weapon not found!"));
+        }
+    }
+}
 
-	AActor* Nearest = nullptr;
+void ABotCharacter::ServerBeginPlay_Implementation()
+{
+    BeginPlay();
+}
 
-	for (int i = 0; i < FoundActors.Num(); i++)
-	{
-		if (FoundActors[i] != this)
-		{
-			if (!Nearest)
-			{
-				Nearest = FoundActors[i];
-			}
-			float distOld = Nearest->GetDistanceTo(this);
-			float distNew = FoundActors[i]->GetDistanceTo(this);
-
-			if (distNew < distOld)
-			{
-				Nearest = FoundActors[i];
-			}
-		}
-	}
-	if (Nearest)
-	{
-		
-		UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(GetWorld(), GetActorLocation(), Nearest);
-
-		if (NavPath && NavPath->PathPoints.Num() > 1)
-		{
-			FVector NextPoint = NavPath->PathPoints[1];
-			FVector Direction = NextPoint - GetActorLocation();
-			Direction.Normalize();
-			GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Yellow, GetControlRotation().ToString());
-			FRotator Rotation = Direction.Rotation();
-			Rotation.Pitch = 0;
-			Rotation.Roll = 0;
-			SetActorRotation(Rotation);
-			MoveForward(5);
-		}
-		
-		FVector NearestLoc = Nearest->GetActorLocation();
-		FVector Direction = NearestLoc - GetActorLocation();
-
-		Direction.Normalize();
-		
-
-	}
-
+bool ABotCharacter::ServerBeginPlay_Validate()
+{
+    return true;
 }
--- Source/COOP_Game_v3/Private/BotController.cpp
+++ Source/COOP_Game_v3/Private/BotController.cpp
@@ -2,3 +2,57 @@
 
 
 #include "BotController.h"
+#include "BotCharacter.h"
+#include "Engine/Engine.h"
+#include "Public/SCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "NavigationSystem/Public/NavigationSystem.h"
+#include "NavigationSystem/Public/NavigationPath.h"
+#include "Public/SWeapon.h"
+
+void ABotController::BeginPlay()
+{
+    bSetControlRotationFromPawnOrientation = true;
+}
+
+void ABotController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    if (Role == ROLE_Authority)
+    {
+        TArray<AActor*> FoundActors;
+        
+        UGameplayStatics::GetAllActorsOfClass(GetWorld(), ASCharacter::StaticClass(), FoundActors);
+        
+        AActor* Nearest = nullptr;
+        
+        for (int i = 0; i < FoundActors.Num(); i++)
+        {
+            if (FoundActors[i] != this && !Cast<ABotCharacter>(FoundActors[i]))
+            {
+                if (!Nearest)
+                {
+                    Nearest = FoundActors[i];
+                }
+                float distOld = Nearest->GetDistanceTo(this);
+                float distNew = FoundActors[i]->GetDistanceTo(this);
+                
+                if (distNew < distOld)
+                {
+                    Nearest = FoundActors[i];
+                }
+            }
+        }
+        if (Nearest) {
+//            K2_SetFocus(Nearest);
+            SetFocalPoint(Nearest->GetActorLocation());
+            
+        } else {
+            UE_LOG(LogTemp, Warning, TEXT("(BOTController) Tick failed: Nearest not found"));
+        }
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("(BOTController) Tick failed: not server auth"));
+    }
+}
