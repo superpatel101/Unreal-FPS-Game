8c9aab9faa13401d4c77b9abd4050b3741ea3419
Aaryan Patel <aaryanv2002@gmail.com>
2019-06-14 10:43:58 -0400

    wasd

--- Source/COOP_Game_v3/Private/SuperSpeedActor.cpp
+++ Source/COOP_Game_v3/Private/SuperSpeedActor.cpp
@@ -7,20 +7,19 @@
 #include "Public/SCharacter.h"
 #include "Engine/Engine.h"
 
-ASuperSpeedActor::ASuperSpeedActor()
+ASuperSpeedActor::ASuperSpeedActor()//the powerup makes the actor go twice as fast
 {
 	speedMultiplier = 2.0f;
 }
 
-void ASuperSpeedActor::OnActivated(AActor* OtherActor)
+void ASuperSpeedActor::OnActivated(AActor* OtherActor)//when powerup is activated
 {
 	Super::OnActivated(OtherActor);
 	ASCharacter* OtherPlayer = Cast<ASCharacter>(OtherActor);
 
 	if (OtherPlayer && Role == ROLE_Authority)
 	{
-		OtherPlayer->GetCharacterMovement()->MaxWalkSpeed *= speedMultiplier;
-		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("should be faster...."));
+		OtherPlayer->GetCharacterMovement()->MaxWalkSpeed *= speedMultiplier;//adjust speed
 	} else
 	{
 		if (GEngine)
@@ -31,12 +30,12 @@ void ASuperSpeedActor::OnActivated(AActor* OtherActor)
 	
 }
 
-void ASuperSpeedActor::OnExpired(AActor* OtherActor)
+void ASuperSpeedActor::OnExpired(AActor* OtherActor)//when the speed powerup is finished
 {
 	Super::OnExpired(OtherActor);
 	ASCharacter* OtherPlayer = Cast<ASCharacter>(OtherActor);
 
-	if (OtherPlayer)
+	if (OtherPlayer)//just divides max speed by speed multiplier to get original speed
 	{
 		OtherPlayer->GetCharacterMovement()->MaxWalkSpeed /= speedMultiplier;
 	}
